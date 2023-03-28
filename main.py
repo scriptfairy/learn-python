e645cfb69de971fe918e8c039dff61bbc9cae552
Christine Al-Thifairy <chris@jzoo.com>
2023-03-29 06:04:22 +1100

    Add main.py for testing individual modules

---
+++ main.py
@@ -0,0 +1,1 @@
+import app.week1.exercise5
---
+++ tests/week1/test_exercise5.py
@@ -0,0 +1,20 @@
+import pytest
+import app.week1.exercise5 as exercise5
+
+# FIX
+# def test_percentage_conversion_of_all_zero_values():
+#     l = [0, 0, 0]
+#     assert exercise5.percentage_conversion(l) == [0, 0, 0]
+
+# FIX
+# def test_percentage_conversion_with_negative_values_summing_to_zero():
+#     l = [-1, -1]
+#     assert exercise5.percentage_conversion(l) == [0, 0]
+
+def test_percentage_conversion_with_a_zero_value():
+    l = [1, 0, 3]
+    assert exercise5.percentage_conversion(l) == [25, 0, 75]
+
+def test_percentage_conversion():
+    l = [1, 2, 3]
+    assert exercise5.percentage_conversion(l) == [17, 33, 50]
