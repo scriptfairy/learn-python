edc4e718e9eb62eacd62db48cf18ceed355f01bc
Christine Al-Thifairy <chris@jzoo.com>
2023-03-29 03:44:05 +1100

    Just update

---
+++ app/week1/Python exercises week 1.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Exercises - Week 1
+
+# ## Purpose
+# The purpose of these exercises is to get familiar with how python works, with or without needing to import libraries. This set of exercises will help guide you to use basic python operations, and create functions.
+
+# ## Exercise 0
+# 
+# Print "Hello World!"
+# 
+# **Hint**: You'll need to use Python's **print** function
+
+# In[4]:
+
+
+# Insert your answer below:
+
+
+# ## Exercise 1
+# 
+# Multiply 5 by 7
+# 
+# - You may or may not save this as a variable. Up to you
+
+# In[5]:
+
+
+# Insert your answer below:
+
+
+# ## Exercise 2
+# 
+# Multiply 5 by 7 then square-root the result
+# 
+# - You may or may not save this as a variable. Up to you
+
+# In[6]:
+
+
+# Insert your answer below:
+
+
+# ## Exercise 3
+# 
+# Create a list of 10 random numbers
+# 
+# **Hint**: You will need to look up how to generate a random 10 numbers first, then save those numbers into a list. Alternatively, create an empty list first, then fill it with 10 random numbers
+
+# In[7]:
+
+
+# Insert your answer below:
+
+
+# ## Exercise 4
+# 
+# Sort the above list in descending order.
+# 
+# - Don't do this manually! There is a Python function for this
+
+# In[8]:
+
+
+# Insert your answer below:
+
+
+# ## Exercise 5
+# 
+# Convert every item in the above list into a percentage out of 100 
+# 
+# - doesn't matter if the percentage is over 100, under 100, or negative
+
+# In[9]:
+
+
+# Insert your answer below:
+
+
+# ## Exercise 6
+# 
+# Create a list of 10 random strings of different lengths. You can use city or country names, proper names, animals, anything!
+
+# In[10]:
+
+
+# Insert your answer below:
+
+
+# ## Exercise 7
+# 
+# Create a function to sort the list above alphabetically
+# 
+# - This question needs you to create your own function, which has a sorting mechanism within it. You may need to look this up
+
+# In[11]:
+
+
+# Insert your answer below:
+
+
+# ## Exercise 8
+# 
+# Create a function to count the vowels in each word in your list
+# 
+# - As with Exercise 7, you will need to make your own function
+
+# In[12]:
+
+
+# Insert your answer below:
+
+
+# ## Exercise 9
+# 
+# Write a function in Python that accepts a credit card number. It should return a string where all the characters are hidden with an asterisk except the last four. For example, if the function gets sent "123456789", then it should return “6789”
+
+# In[13]:
+
+
+# Insert your answer below:
+
+
+# ## Exercise 10
+# 
+# Write a Python function that accepts three parameters. The first parameter is an integer. The second is one of the following mathematical operators: +, -, /, or . The third parameter will also be an integer.
+# 
+# The function should perform a calculation and return the results. For example, if the function is passed 6 and 4, it should return 24.
+
+# In[14]:
+
+
+# Insert your answer below:
+
+
+# ## Exercise 11
+# 
+# Create a function in Python that accepts two parameters. The first should be the full price of an item as an integer. The second should be the discount percentage as an integer.
+# 
+# The function should return the price of the item after the discount has been applied. For example, if the price is 100 and the discount is 20, the function should return 80.
+
+# In[15]:
+
+
+# Insert your answer below:
+
+
+# ## Exercise 12
+# 
+# Write a function in Python that accepts a list of any length that contains a mix of non-negative integers and strings. The function should return a list with only the integers in the original list in the same order.
+
+# In[16]:
+
+
+# Insert your answer below:
+
+
+# ## Exercise 13
+# 
+# Create a Python function that accepts a string. The function should return a string, with each character in the original string doubled. If you send the function “now” as a parameter, it should return “nnooww,” and if you send “123a!”, it should return “112233aa!!”.
+
+# In[17]:
+
+
+# Insert your answer below:
+
